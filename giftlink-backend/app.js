bc7478581c65f80ca6cdec9f1c7e10a6b38802f4
johnwendel <john.alan.wendel@gmail.com>
2024-12-11 18:55:58 -0500

    backend updates

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -23,10 +23,12 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
+const giftRoutes = require('./routes/giftRoutes');
+app.use('./api/gifts', giftsRoute);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+const searchRoutes = require('./routes/searchRoutes');
+app.use('/api/search', searchRoutes);
 
 
 const pinoHttp = require('pino-http');
